f932ae768b23f3c626ac582b030c0657d1e45df4
Andrew <azong440@umd.edu>
2025-07-07 16:53:12 -0400

    Added Theme Button Functionality and Themes

--- app/src/main/java/com/example/group_project/MainActivity.kt
+++ app/src/main/java/com/example/group_project/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.group_project
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var localGameHistoryLV : ListView
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Retrieve Color Theme from preferences before creation
+        sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        if(!sharedPref.contains("colorTheme")){
+            sharedPref.edit().putString("colorTheme", "light").apply()
+        }
+        when {
+            sharedPref.getString("colorTheme","light")!! == "light" -> setTheme(R.style.Theme_lightTheme)
+            sharedPref.getString("colorTheme","light")!! == "dark" -> setTheme(R.style.Theme_darkTheme)
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -20,8 +34,20 @@ class MainActivity : AppCompatActivity() {
             insets
 
         }
+        setupThemeButtons()
+        localGameHistoryLV = findViewById(R.id.local_history)
+    }
 
-       localGameHistoryLV = findViewById(R.id.local_history)
+    private fun setupThemeButtons() {
+        findViewById<AppCompatButton>(R.id.light_theme_button).setOnClickListener {
+            sharedPref.edit().putString("colorTheme", "light").apply()
+            recreate() // this will trigger onCreate and apply the theme there
+        }
+
+        findViewById<AppCompatButton>(R.id.dark_theme_button).setOnClickListener {
+            sharedPref.edit().putString("colorTheme", "dark").apply()
+            recreate()
+        }
     }
 
 }
