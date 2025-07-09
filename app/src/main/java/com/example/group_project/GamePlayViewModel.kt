7618adde632b5ab3d4ae248d47e16bae4d2162a8
vera-umd <swaira@terpmail.umd.edu>
2025-07-08 22:56:47 -0400

    added seekbar and fixed balance so it should be consistent

--- app/src/main/java/com/example/group_project/GamePlayViewModel.kt
+++ app/src/main/java/com/example/group_project/GamePlayViewModel.kt
@@ -10,7 +10,9 @@ import androidx.lifecycle.MutableLiveData
 import com.example.group_project.BlackJackModel
 class GamePlayViewModel(app: Application): AndroidViewModel(app) {
 
-    private val game = BlackJackModel(app, startBalance = 1000)
+    private val prefs = app.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+    private val startingBalance = prefs.getInt("balance",1000)
+    private val game = BlackJackModel(app, startingBalance )
 
     private val _player_cards = MutableLiveData<List<Card>>(emptyList())
     val player_cards: LiveData<List<Card>> = _player_cards
@@ -39,6 +41,7 @@ class GamePlayViewModel(app: Application): AndroidViewModel(app) {
         _player_cards.value = game.player.cards
         _dealer_cards.value = game.dealer.cards
         _balance.value = game.balance
+        prefs.edit().putInt("balance",game.balance).apply()
 
     }
 
@@ -47,6 +50,7 @@ class GamePlayViewModel(app: Application): AndroidViewModel(app) {
         _player_cards.value = game.player.cards
         _dealer_cards.value = game.dealer.cards
         _balance.value = game.balance
+        prefs.edit().putInt("balance",game.balance).apply()
     }
 
 
--- app/src/main/java/com/example/group_project/GameplayActivity.kt
+++ app/src/main/java/com/example/group_project/GameplayActivity.kt
@@ -7,6 +7,7 @@ import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -23,7 +24,8 @@ class GameplayActivity: AppCompatActivity() {
     private lateinit var standBTN: Button
     private lateinit var backBTN: Button
     private lateinit var balanceTextView: TextView
-    private lateinit var betInput: EditText
+    private lateinit var betSeekBar: SeekBar
+    private lateinit var bettv : TextView
     private lateinit var dealButton: Button
 
 
@@ -58,7 +60,8 @@ class GameplayActivity: AppCompatActivity() {
         standBTN =findViewById(R.id.stand_button)
         backBTN = findViewById(R.id.leave_game_button)
         balanceTextView = findViewById(R.id.game_balance)
-        betInput = findViewById(R.id.bet_input)
+        betSeekBar = findViewById(R.id.bet_seekbar)
+        bettv = findViewById(R.id.betTV)
         dealButton = findViewById(R.id.DealBtn)
 
 
@@ -72,7 +75,21 @@ class GameplayActivity: AppCompatActivity() {
         vm.player_cards.observe(this) {cards ->
             Log.d("MainActivity","Player cards list size = ${cards.size}")
             playerAdapter.update(cards)  }
-        vm.balance.observe(this) {bal -> balanceTextView.text = "$$bal"  }
+        vm.balance.observe(this) {bal ->
+            balanceTextView.text = "$$bal"
+             betSeekBar.max = bal
+            if(betSeekBar.progress> bal ) betSeekBar.progress = bal
+        }
+        betSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val bet = maxOf(1, progress)
+                bettv.text = "Bet:$$bet"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+        }   )
+        betSeekBar.progress = 1
 
         vm.result.observe(this){ result ->
             result?.let{ round->
@@ -97,24 +114,22 @@ class GameplayActivity: AppCompatActivity() {
                     .apply()
                 hitBTN.isEnabled =false
                 standBTN.isEnabled = false
+                dealButton.isEnabled = true
             }
         }
 
         dealButton.setOnClickListener {
-            val bet = betInput.text.toString().toIntOrNull()
-            if(bet == null|| bet<= 0){
-                Toast.makeText(this, "Enter a valid bet", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            if (bet > (vm.balance.value?:0)){
-                Toast.makeText(this, "Bet cannot exceed balance", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
+            val bet = maxOf(1, betSeekBar.progress)
 
             vm.startRound(bet)
 
+
+
+
             hitBTN.isEnabled = true
             standBTN.isEnabled = true
+
+            dealButton.isEnabled= false
         }
         hitBTN.setOnClickListener {vm.hit() }
         standBTN.setOnClickListener { vm.stand() }
